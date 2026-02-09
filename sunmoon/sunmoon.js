b5adc8addde8a87132d82439af87ba2bf21649dc
eivorie <126474650+eivorie@users.noreply.github.com>
2026-02-09 13:44:59 +0100

    Update sunmoon.js

--- sunmoon/sunmoon.js
+++ sunmoon/sunmoon.js
@@ -3,11 +3,25 @@ const cards = document.querySelectorAll(".sunmoon-card");
 const switches = document.querySelectorAll(".sunmoon-switch");
 
 function setMode(mode) {
+  // 1. changer le monde (jour / nuit)
   body.classList.remove("mode-moon", "mode-sun");
   body.classList.add(`mode-${mode}`);
 
+  // 2. activer la bonne carte
   cards.forEach(card => {
     card.classList.toggle(
       "is-active",
       card.classList.contains(`card-${mode}`)
     );
+  });
+}
+
+// 3. clic sur les boutons
+switches.forEach(btn => {
+  btn.addEventListener("click", () => {
+    setMode(btn.dataset.target);
+  });
+});
+
+// 4. état initial
+setMode("moon");
